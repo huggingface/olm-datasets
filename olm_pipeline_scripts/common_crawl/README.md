9b4aefb6e1acfd15a23a226be192cbb323b9692a
TristanThrush <tristan@huggingface.co>
2022-09-26 22:07:48 +0000

    cc download bugfix

--- olm_pipeline_scripts/common_crawl/README.md
+++ olm_pipeline_scripts/common_crawl/README.md
@@ -1,7 +1,7 @@
 Follow these steps to pull, process, and push to the Hugging Face hub your very own Common Crawl pretraining dataset, replacing the arguments to suit your needs:
 
 1. `bash download_pipeline_processing_models.sh`
-2. `python download_common_crawl.py --common_crawl_snapshots=CC-MAIN-2022-33 CC-MAIN-2022-27 --common_crawl_snapshot_offsets=78500 78500 --download_dir=common_crawl_wet_downloads --num_proc=96`
+2. `python download_common_crawl.py --common_crawl_snapshots CC-MAIN-2022-33 CC-MAIN-2022-27 --common_crawl_snapshot_offsets 78500 78500 --download_dir=common_crawl_wet_downloads --num_proc=96`
 3. `python get_raw_dataset.py --common_crawl_download_dir=common_crawl_wet_downloads --output_dataset_name=Tristan/cc-english --num_proc=96`
 4. `python remove_wikipedia_urls.py --input_dataset_name=Tristan/cc-english --output_dataset_name=Tristan/cc-english-and-wikipedia-removed --url_column=url --num_proc=96`
 5. `python filter_for_only_updated_websites.py --input_dataset_name=Tristan/cc-english-and-wikipedia-removed --output_dataset_name=Tristan/cc-english-and-wikipedia-removed-and-updated --text_column=text --timestamp_column=timestamp --url_column=url --num_proc=96`
--- olm_pipeline_scripts/common_crawl/download_common_crawl.py
+++ olm_pipeline_scripts/common_crawl/download_common_crawl.py
@@ -12,20 +12,18 @@ args = parser.parse_args()
 run(f"mkdir {args.download_dir}", shell=True)
 for index in range(len(args.common_crawl_snapshots)):
     # Download the data for a certian common crawl snapshot
-    run(f"mkdir ungoliant_downloads-{str(index)}", shell=True)
-    run("wget https://data.commoncrawl.org/crawl-data/$new_crawl/wet.paths.gz", shell=True)
+    download_dir_name = f"ungoliant_downloads-{args.common_crawl_snapshots[index]}"
+    run(f"mkdir {download_dir_name}", shell=True)
+    run(f"wget https://data.commoncrawl.org/crawl-data/{args.common_crawl_snapshots[index]}/wet.paths.gz", shell=True)
     run("gzip -d wet.paths.gz", shell=True)
     wet_paths_name = f"wet-{args.common_crawl_snapshots[index]}.paths"
     run(f"mv wet.paths {wet_paths_name}", shell=True)
-    download_dir_name = f"ungoliant_downloads-{args.common_crawl_snapshots[index]}"
-    run(f"ungoliant download -t={args.num_proc} -o={args.common_crawl_offsets[index]} {wet_paths_name} {download_dir_name}", shell=True)
+    run(f"ungoliant download -t={args.num_proc} -o={args.common_crawl_snapshot_offsets[index]} {wet_paths_name} {download_dir_name}", shell=True)
     run(f"rm {wet_paths_name}", shell=True)
 
     # Now, add 0's to the filename for every downloaded file. We want the number of 0's to be different than those from another common crawl snapshot
     # because we want every file to have a unique name accross multiple snapshot downloads.
-    run(f"cd {download_dir_name}", shell=True)
-    run(f"for f in * ; do mv \"$f\" {'0'*index}\"$f\" ; done", shell=True)
-    run("cd ../", shell=True)
+    run(f"cd {download_dir_name} && for f in * ; do mv \"$f\" {'0'*index}\"$f\" ; done", shell=True)
 
     # Now we can move the downloaded files into the main download dir which has the downloads from the rest of this for loop.
     run("mv {download_dir_name}/* {args.download_dir}/", shell=True)

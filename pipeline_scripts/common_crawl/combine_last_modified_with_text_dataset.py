a322058f73239da1a726116becc7c27cf36b3a40
TristanThrush <tristan@huggingface.co>
2022-11-04 01:40:18 +0000

    update combine_last_modified_with_text_datasets.py so it doesn't make memory explode

--- pipeline_scripts/common_crawl/combine_last_modified_with_text_dataset.py
+++ pipeline_scripts/common_crawl/combine_last_modified_with_text_dataset.py
@@ -2,10 +2,7 @@ from datasets import load_dataset, load_from_disk
 import argparse
 from multiprocessing import Manager
 from tqdm import tqdm
-from os import path, mkdir
-import pickle
-from tqdm import tqdm
-from shutil import rmtree
+import uuid
 
 parser = argparse.ArgumentParser(description="This script takes in a text dataset with crawl timestamps and urls, and then a last-modified dataset with crawl timestamps and urls. It uses the shared urls and crawl timestamps to add last-modified timestamps to the text dataset.")
 parser.add_argument("--text_dataset_name", required=True)
@@ -17,16 +14,10 @@ parser.add_argument("--last_modified_timestamp_column", required=True)
 parser.add_argument("--crawl_timestamp_column", required=True)
 parser.add_argument("--url_column", required=True)
 parser.add_argument("--num_proc", type=int, required=True)
-parser.add_argument("--tmp_dir", default=".tmp_combine_last_modified_with_text_dataset")
 parser.add_argument("--load_text_dataset_from_hub_instead_of_disk", action="store_true", help="Whether to load the text dataset from the Hugging Face hub instead of the disk (default is the disk).")
 parser.add_argument("--load_last_modified_dataset_from_hub_instead_of_disk", action="store_true", help="Whether to load the last modified dataset from the Hugging Face hub instead of the disk (default is the disk).")
 parser.add_argument("--push_to_hub", action="store_true")
 args = parser.parse_args()
-
-if path.exists(args.tmp_dir):
-    rmtree(args.tmp_dir)
-
-mkdir(args.tmp_dir)
 
 if args.load_text_dataset_from_hub_instead_of_disk:
     if args.text_dataset_split is None:
@@ -50,6 +41,7 @@ else:
     else:
         last_modified_ds = load_from_disk(args.last_modified_dataset_name)[args.last_modified_dataset_split]
 
+
 with Manager() as manager:
     shared_list = manager.list()
     def build_last_modified_dict(examples):
@@ -64,20 +56,10 @@ with Manager() as manager:
     for last_modified_dict in tqdm(shared_list):
         aggregate_last_modified_dict |= last_modified_dict
 
-    pickle.dump(aggregate_last_modified_dict, open(path.join(args.tmp_dir, "last_modified_dict.pkl"), "wb"))
-
-def add_last_modified(examples):
-    last_modified_dict = pickle.load(open(path.join(args.tmp_dir, "last_modified_dict.pkl"), "rb"))
-    last_modified_timestamps = []
-    for url, crawl_timestamp in zip(examples[args.url_column], examples[args.crawl_timestamp_column]):
-        last_modified_timestamps.append(last_modified_dict.get((url, crawl_timestamp), None))
-    return {args.last_modified_timestamp_column: last_modified_timestamps}
-
-text_ds = text_ds.map(add_last_modified, num_proc=args.num_proc, batched=True, batch_size=len(text_ds) // args.num_proc)
+# Set the new fingerprint manually so the map function doesn't take forever hashing the huge aggregate_last_modified_dict.
+text_ds = text_ds.map(lambda example: {args.last_modified_timestamp_column: aggregate_last_modified_dict.get((example[args.url_column], example[args.crawl_timestamp_column]), None)}, new_fingerprint=str(uuid.uuid4()))
 
 text_ds.save_to_disk(args.output_dataset_name)
-
-rmtree(args.tmp_dir)
 
 if args.push_to_hub:
     text_ds.push_to_hub(args.output_dataset_name)
